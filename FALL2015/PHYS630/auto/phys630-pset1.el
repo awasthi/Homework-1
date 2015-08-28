85c6f349fb271dc74661624d0f6e6241ffa0c094
Карлос әфәнде <cemiliosal@gmail.com>
2015-08-28 15:22:35 -0400

    Add FALL2015

---
+++ FALL2015/PHYS630/auto/phys630-pset1.el
@@ -0,0 +1,9 @@
+(TeX-add-style-hook
+ "phys630-pset1"
+ (lambda ()
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (LaTeX-add-labels
+    "eq:electric-field-conductor")))
+
