85ba2db79a997bf093d5e499a41f2b983bf83ac1
Карлос әфәнде <cemiliosal@gmail.com>
2015-09-03 09:25:39 -0400

    Add FALL2015

---
+++ FALL2015/auto/FALL15-Notes.el
@@ -0,0 +1,72 @@
+(TeX-add-style-hook
+ "FALL15-Notes"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("memoir" "article" "oneside" "10pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("xcolor" "dvipsnames") ("enumitem" "inline")))
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (TeX-run-style-hooks
+    "latex2e"
+    "MA557/notes/atiyah-mcdonald"
+    "MA571/notes/munkres"
+    "memoir"
+    "memoir10"
+    "geometry"
+    "xcolor"
+    "graphicx"
+    "multicol"
+    "enumitem"
+    "listings"
+    "mleftright"
+    "microtype"
+    "amsthm"
+    "amssymb"
+    "mathtools"
+    "lualatex-math"
+    "unicode-math")
+   (TeX-add-symbols
+    '("restr" 2)
+    "id"
+    "im"
+    "GL"
+    "GO"
+    "OO"
+    "SL"
+    "SO"
+    "CC"
+    "CP"
+    "GG"
+    "NN"
+    "PP"
+    "QQ"
+    "RP"
+    "RR"
+    "Sphere"
+    "Torus"
+    "ZZ"
+    "documentauthor"
+    "documenttitle"
+    "coursename"
+    "shorttitle"
+    "documentsubject"
+    "authoremail")
+   (LaTeX-add-environments
+    "theorem"
+    "proposition"
+    "corollary"
+    "claim"
+    "lemma"
+    "axiom"
+    "definition"
+    "example"
+    "examples"
+    "exercise"
+    "problem"
+    "remark"
+    "remarks"
+    "observation"
+    "observations")))
+
