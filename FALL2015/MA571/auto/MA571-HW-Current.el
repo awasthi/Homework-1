d12043b44d8783feb474ab5d656fc5ce79fddbb6
Карлос әфәнде <cemiliosal@gmail.com>
2015-09-14 22:48:47 -0400

    Changes Fall 2015

--- FALL2015/MA571/auto/MA571-HW-Current.el
+++ FALL2015/MA571/auto/MA571-HW-Current.el
@@ -10,7 +10,7 @@
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
    (TeX-run-style-hooks
     "latex2e"
-    "psets/ma571-pset3"
+    "psets/ma571-pset4"
     "memoir"
     "memoir10"
     "geometry"
