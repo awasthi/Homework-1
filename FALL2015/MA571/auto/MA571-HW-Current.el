9eb4b5be520d82b386aab5b012a806be64ec4fd6
Карлос әфәнде <cemiliosal@gmail.com>
2015-09-20 21:04:20 -0400

    Changes Fall 2015

--- FALL2015/MA571/auto/MA571-HW-Current.el
+++ FALL2015/MA571/auto/MA571-HW-Current.el
@@ -4,7 +4,7 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("memoir" "article" "oneside" "10pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("xcolor" "dvipsnames") ("enumitem" "inline") ("titlesec" "explicit")))
+                     '(("xcolor" "dvipsnames") ("enumitem" "inline") ("fontenc" "T1") ("titlesec" "explicit")))
    (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
@@ -24,7 +24,8 @@
     "amsthm"
     "amssymb"
     "mathtools"
-    "unicode-math"
+    "fontenc"
+    "lmodern"
     "titlesec")
    (TeX-add-symbols
     '("clsr" 1)
