6c274374865a19bce84c135fdba59c9275faf8af
Карлос әфәнде <cemiliosal@gmail.com>
2015-11-24 18:00:16 -0500

    Latest changes by MA571 homework 13.

--- FALL2015/MA571/auto/MA571-HW-Current.el
+++ FALL2015/MA571/auto/MA571-HW-Current.el
@@ -10,7 +10,7 @@
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
    (TeX-run-style-hooks
     "latex2e"
-    "psets/ma571-pset12"
+    "psets/ma571-pset13"
     "memoir"
     "memoir10"
     "geometry"
---
+++ FALL2015/MA571/auto/ma571-pset13.el
@@ -0,0 +1,9 @@
+(TeX-add-style-hook
+ "ma571-pset13"
+ (lambda ()
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (LaTeX-add-environments
+    '("problem" LaTeX-env-args ["argument"] 0))))
+
