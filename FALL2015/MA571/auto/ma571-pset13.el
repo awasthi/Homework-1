f9559950fbbbe01ec172b46bd3b4708ec3adaadb
Carlos Salinas <cemiliosal@gmail.com>
2015-11-24 23:50:29 -0500

    Latest changes to MA571 homework 13.

--- FALL2015/MA571/auto/ma571-pset13.el
+++ FALL2015/MA571/auto/ma571-pset13.el
@@ -4,6 +4,8 @@
    (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (LaTeX-add-labels
+    "eq:1")
    (LaTeX-add-environments
     '("problem" LaTeX-env-args ["argument"] 0))))
 
