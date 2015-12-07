13400ba64c9938e76a2680f734921d3aaad43ae6
Карлос әфәнде <cemiliosal@gmail.com>
2015-12-07 15:35:15 -0500

    Removed some unecessary files added by AUCTeX.

--- FALL2015/MA553/auto/MA553-AUG10.el
+++
@@ -1,9 +0,0 @@
-(TeX-add-style-hook
- "MA553-AUG10"
- (lambda ()
-   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
-   (LaTeX-add-environments
-    '("problem" LaTeX-env-args ["argument"] 0))))
-
--- FALL2015/MA571/auto/ma571-midterm-2.el
+++
@@ -1,9 +0,0 @@
-(TeX-add-style-hook
- "ma571-midterm-2"
- (lambda ()
-   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
-   (LaTeX-add-environments
-    '("problem" LaTeX-env-args ["argument"] 0))))
-
